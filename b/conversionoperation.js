921106d8c17e1dc53479d1ffd7d34ff8f450ec1a
aditya4635 <159606279+aditya4635@users.noreply.github.com>
2024-08-26 11:53:28 +0000

    changes

---
+++ b/conversionoperation.js
@@ -0,0 +1,6 @@
+let score=33
+//"33"=33
+//"33abc" = NaN
+//type of NaN=NUMBER
+//true= 1, false =0
+//=== strict check 
---
+++ b/datatypessum.js
@@ -0,0 +1,32 @@
+//primitive
+//7 types
+//string
+/*number
+boolean
+null
+undefined
+symbol
+bigint
+
+
+reference datatype/non primitive
+arrays,objects,functions
+
+dynamically typed languge
+*/
+//using symbol
+const id=symbol('123')
+//for bigint
+const bignum=276428759275n
+//arrays
+const heros=['hero1','hero2'];
+//object
+let myobj={
+    name:"hitesh",
+    age: 22,
+}
+
+//function
+const myfun= function(){
+    console.log('hello world');
+}
